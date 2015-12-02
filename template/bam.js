bf1e72542fd50c3aab61f83a34cca23ddb2eef9f
Nicolas Djambazian <nicolas@djambazian.fr>
2015-12-03 00:59:53 +0100

    update template file

--- template/bam.js
+++ template/bam.js
@@ -1,7 +1,18 @@
+/*
+ * This file is a configuration for the bam command line tool
+ * @see https://github.com/bamlab/bam-cmd
+ */
 
-
+var exec = require('child_process').exec;
 
 module.exports = {
 	scriptVersion: '0.1.0',
 	dirName: 'repository-dir-name'
+	linkedRepos: [],
+	install: function() {
+		// run at the installation. Do not accept any argument
+		exec('npm install', function(err, stdin, stderr) {
+			console.log(stdin);	
+		});
+	},
 };
